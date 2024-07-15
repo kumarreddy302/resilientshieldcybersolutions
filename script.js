4b15b707b941175a01de51496e78c3ba171d2c2e
Shanmukha Sai Kumar Reddy Kovvuri <84006155+kumarreddy302@users.noreply.github.com>
2024-07-15 21:24:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('menuIcon').addEventListener('click', function () {
+    var navLinks = document.getElementById('navLinks');
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+});
+
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = 'flex';
+}
+
+function closePopup(popupId) {
+    document.getElementById(popupId).style.display = 'none';
+}
